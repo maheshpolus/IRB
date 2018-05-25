8a9ad12ff94312cefeb6c70f7a0c645d69f657fe
mrudulpolus <mrudul@polussoftware.com>
2018-05-25 17:09:26 +0530

    front end commi

---
+++ mit-irb-frontend/src/app/expanded-view/expanded-view.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+
+@Injectable()
+export class ExpandedViewService {
+
+  constructor(private _http: HttpClient) { }
+ 
+  getExpandedList(params) {
+      return this._http.post('/mit-irb/getExpandedSnapShotView', params);
+  }
+}

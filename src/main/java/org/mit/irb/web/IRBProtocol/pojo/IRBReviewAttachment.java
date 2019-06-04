ff29645b796f558264d32b4bdfaf26dd1bb0faa7
mrudul <mrudul@polussoftware.com>
2019-06-04 15:50:40 +0530

    Review Comments and attachments

---
+++ src/main/java/org/mit/irb/web/IRBProtocol/pojo/IRBReviewAttachment.java
@@ -0,0 +1,76 @@
+package org.mit.irb.web.IRBProtocol.pojo;
+
+public class IRBReviewAttachment {
+	private String description;
+	private Integer fileDataId;
+	private String updateUser;
+	private String fullName;
+	private Integer adminReviewId;
+	private Integer commentId;
+	private Integer attachmentId;
+	private String FileName;
+	private String mimeType;
+	private String updateTimeStamp;
+	
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Integer getFileDataId() {
+		return fileDataId;
+	}
+	public void setFileDataId(Integer fileDataId) {
+		this.fileDataId = fileDataId;
+	}
+	public String getUpdateUser() {
+		return updateUser;
+	}
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public Integer getAdminReviewId() {
+		return adminReviewId;
+	}
+	public void setAdminReviewId(Integer adminReviewId) {
+		this.adminReviewId = adminReviewId;
+	}
+	public Integer getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(Integer commentId) {
+		this.commentId = commentId;
+	}
+	public Integer getAttachmentId() {
+		return attachmentId;
+	}
+	public void setAttachmentId(Integer attachmentId) {
+		this.attachmentId = attachmentId;
+	}
+	public String getFileName() {
+		return FileName;
+	}
+	public void setFileName(String fileName) {
+		FileName = fileName;
+	}
+	public String getMimeType() {
+		return mimeType;
+	}
+	public void setMimeType(String mimeType) {
+		this.mimeType = mimeType;
+	}
+	public String getUpdateTimeStamp() {
+		return updateTimeStamp;
+	}
+	public void setUpdateTimeStamp(String updateTimeStamp) {
+		this.updateTimeStamp = updateTimeStamp;
+	}
+
+}
---
+++ src/main/java/org/mit/irb/web/IRBProtocol/pojo/IRBReviewComment.java
@@ -0,0 +1,115 @@
+package org.mit.irb.web.IRBProtocol.pojo;
+
+import java.util.List;
+
+public class IRBReviewComment {
+	private Integer submissionId;
+	private Integer sequenceNumber;
+	private String commentDescription;
+	private String protocolNumber;
+	private String udpateUser;
+	private String fullName;
+	private Integer commentId;
+	private Integer adminReviewId;
+	private Integer attachmentId;
+	private String updateTimestamp;
+	private Integer protocolId;
+	private List<IRBReviewAttachment> reviewAttachments;
+	
+	public Integer getSubmissionId() {
+		return submissionId;
+	}
+
+	public void setSubmissionId(Integer submissionId) {
+		this.submissionId = submissionId;
+	}
+
+	public Integer getSequenceNumber() {
+		return sequenceNumber;
+	}
+
+	public void setSequenceNumber(Integer sequenceNumber) {
+		this.sequenceNumber = sequenceNumber;
+	}
+
+	public String getProtocolNumber() {
+		return protocolNumber;
+	}
+
+	public void setProtocolNumber(String protocolNumber) {
+		this.protocolNumber = protocolNumber;
+	}
+
+	public String getUdpateUser() {
+		return udpateUser;
+	}
+
+	public void setUdpateUser(String udpateUser) {
+		this.udpateUser = udpateUser;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public Integer getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(Integer commentId) {
+		this.commentId = commentId;
+	}
+
+	public Integer getAdminReviewId() {
+		return adminReviewId;
+	}
+
+	public void setAdminReviewId(Integer adminReviewId) {
+		this.adminReviewId = adminReviewId;
+	}
+
+	public Integer getAttachmentId() {
+		return attachmentId;
+	}
+
+	public void setAttachmentId(Integer attachmentId) {
+		this.attachmentId = attachmentId;
+	}
+
+	public String getUpdateTimestamp() {
+		return updateTimestamp;
+	}
+
+	public void setUpdateTimestamp(String updateTimestamp) {
+		this.updateTimestamp = updateTimestamp;
+	}
+
+	public Integer getProtocolId() {
+		return protocolId;
+	}
+
+	public void setProtocolId(Integer protocolId) {
+		this.protocolId = protocolId;
+	}
+
+	public List<IRBReviewAttachment> getReviewAttachments() {
+		return reviewAttachments;
+	}
+
+	public void setReviewAttachments(List<IRBReviewAttachment> reviewAttachments) {
+		this.reviewAttachments = reviewAttachments;
+	}
+
+	public String getCommentDescription() {
+		return commentDescription;
+	}
+
+	public void setCommentDescription(String commentDescription) {
+		this.commentDescription = commentDescription;
+	}
+
+}

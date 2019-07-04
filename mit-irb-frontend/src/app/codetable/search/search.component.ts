6eedc9c3284cc722ef39add323093d889d9214c6
mrudul <mrudul mrudul@polussoftware.com>
2019-07-04 19:33:31 +0530

    Commit for changes in code table

--- mit-irb-frontend/src/app/codetable/search/search.component.ts
+++ mit-irb-frontend/src/app/codetable/search/search.component.ts
@@ -6,110 +6,93 @@ import * as _ from 'lodash';
 import { SearchService } from './search.service';
 
 @Component({
-  selector   : 'app-search',
-  templateUrl: './search.component.html',
-  styleUrls  : ['./search.component.css']
+    selector: 'app-search',
+    templateUrl: './search.component.html',
+    styleUrls: ['./search.component.css']
 })
 
 export class SearchComponent implements OnInit {
-
     public dataService: CompleterData;
     codeTableProperty: any = {};
     tableProperty: any = {};
     checkedList: any = {};
-    isViewEnable: any = [];
+    codeTableValues: any = [];
     groupNames: any = [];
     groupNameSelected: any;
+    SearchTableName: any;
     codeTableLength: number;
     selectedId: number;
-    isCodeTableList;
-    isVisible  = false;
-    SearchTableName ;
+    isVisible = false;
+    isCollapse = false;
     isSavedFlag = {
-        isSaved : null
+        isSaved: null,
+        isgrpNameChange: false
     };
+    helpInfo;
 
     constructor(private _tableService: SearchService,
-                private _completerService: CompleterService) { }
+        private _completerService: CompleterService) { }
 
     ngOnInit() {
         this._tableService.getTableProperties()
-        .subscribe(
-            data => {
-            this.tableProperty = data;
-            this.groupNames    = this.tableProperty.configFile.groups;
-            this.groupNames.forEach(element => {
-                this.isViewEnable.push(true);
-            });
-            this.checkedList[this.groupNames[0]] = true;
-            this.setCodeTableListBox();
-            this.dataService   = this._completerService.local
-                                (this.tableProperty.configFile.codetables,
-                                'group,codetable_name', 'codetable_name');
-            }, err => {
-                console.log('Error in Fetching Data', err);
-        });
+            .subscribe(
+                data => {
+                    this.tableProperty = data;
+                    this.groupNames = this.tableProperty.configFile.groups;
+                    this.checkedList.groupName = this.groupNames[0];
+                    this.dataService = this._completerService.local
+                        (this.tableProperty.configFile.codetables,
+                            'group,codetable_name', 'codetable_name');
+                }, err => {
+                    console.log('Error in Fetching Data', err);
+                });
     }
     /**
      * @param  {} groupName
-     * generates the first ten code table names which are displayed when the check box is clicked.
+     * filters the codetable names with respect to group name,
+     * and returns them on the radio button click.
      */
     getCodeTableList(groupName: any) {
-        this.codeTableLength = _.filter( this.tableProperty.configFile.codetables, {'group': groupName}).length;
-        return _.filter( this.tableProperty.configFile.codetables, {'group': groupName}).slice(0, 10);
-    }
-    /**
-     * @param  {} groupName
-     * generates the full list of codetable names which are displayed when the 'view more' is clicked.
-     */
-    getFullCodeTableList (groupName: any) {
-        return _.filter( this.tableProperty.configFile.codetables, {'group': groupName});
+        if (groupName !== undefined) {
+            return _.filter(this.tableProperty.configFile.codetables, { 'group': groupName });
+        }
     }
     /**
      * @param  {} tablename
-     * filter out properties of searched table selected from search dropdown
+     * filter out properties of searched table selected from search dropdown in the top.
+     * when a table name is selcted from the dropdown collapses the codetable name list which shows under radio button list.
      */
     getSearchedTable(tablename: any) {
         if (tablename != null) {
-            const group          = tablename.originalObject.group;
+            const group = tablename.originalObject.group;
             const codetable_name = tablename.originalObject.codetable_name;
-            this.getSelectedTable(group, codetable_name);
+            if (this.isCollapse === false) {
+                document.getElementById('advancesearch').click();
+            }
+            this.getSelectedTable(group, codetable_name, 1);
             this.getSelectedId(1, codetable_name);
         }
-
     }
     /**
      * @param  {} modulename
      * @param  {} tablename
      * filter out properties of searched table selected from advance search box
+     * isvisible is set for visibility of child component(codetable component).
+     * the selected tables properties are passed into the child component.
+     * defaultdly set the value to '0' to distinguish between the caller of the function 'getSelectedTable()'.
+     * value is used for managing accordion working.
      */
-    getSelectedTable(modulename, tablename) {
+    getSelectedTable(modulename, tablename, value = 0) {
         if (this.isSavedFlag.isSaved === true || this.isSavedFlag.isSaved === null) {
             this.SearchTableName = tablename;
-            this.isVisible         = true;
+            this.isVisible = true;
             this.codeTableProperty = {};
             this.codeTableProperty = this.tableProperty.configFile.codetables.filter(fieldList => (
-                                ( modulename === fieldList.group) && (tablename === fieldList.codetable_name)))[0];
-            setTimeout( () => {
-                const id = document.getElementById('code-table');
-                if (id) {
-                    id.scrollIntoView({behavior : 'smooth'});
-                }
-            }, 0);
-        }
-    }
-    /**
-     * @param  {} id
-     * set isViewEnable according to the view more click
-     */
-    setViewEnable(id: number) {
-        this.groupNames.forEach((value, index) => {
-            if (index === id && this.isViewEnable[index] === true) {
-                this.isViewEnable[index] = false;
-            } else {
-                this.isViewEnable[index] = true;
+                (modulename === fieldList.group) && (tablename === fieldList.codetable_name)))[0];
+            if (value === 0) {
+                document.getElementById('advancesearch').click();
             }
-        });
+        }
     }
     /**
      * @param  {} id
@@ -120,11 +103,18 @@ export class SearchComponent implements OnInit {
         this.groupNameSelected = groupName;
     }
     /**
-     * sets isCodeTableList to true if checkedList contains true values
-     * Used to avoid showing empty box, if checkedList is empty or not containg true values
+     * to set accordion arrow.
      */
-    setCodeTableListBox() {
-        const VALUES = Object.values(this.checkedList);
-        this.isCodeTableList = VALUES.find((key) => key === true);
+    collapseAdvanceSearch() {
+        this.isCollapse === false ? this.isCollapse = true : this.isCollapse = false;
+    }
+    /**
+     * opens accordion when module radio button is clicked.
+     */
+    accordionShow() {
+        this.SearchTableName = '';
+        this.isVisible = false;
+        this.selectedId = -1;
+        document.getElementById('collapseSearch').className = 'show';
     }
 }
